e655d163900d4da45d7a03eb604ab3fcfe901721
Kasim Deliaci <21360859021@ogrenci.btu.edu.tr>
2024-05-30 02:27:31 +0300

    Delete Academicera directory

--- Academicera/templates/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
-        <span class="text-muted">© 2024 Her hakkı saklıdır.</span>
-    </div>
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-        <li class="ms-3"><a class="text-muted" href="https://github.com/KasimDeliaci/Academicera" target="_blank">Github</a></li>
-    </ul>
-</footer>
